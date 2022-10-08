fe2b013740acef1a17e8f8fe8367a462a14dc8d3
simpleDev-core <75375433+simpleDev-core@users.noreply.github.com>
2022-10-08 18:26:02 -0400

    Update Monitor.ino

--- Monitor.ino
+++ Monitor.ino
@@ -7,138 +7,96 @@
 #define PIN_RELAY_4  7 // the Arduino pin, which connects to the IN4 pin of relay module
 
 // Data wire is plugged into digital pin 2 on the Arduino
+#define TEMPA 9
 #define TEMPB 2
 #define TEMPC 11
 #define TEMPD 10
-#define TEMPA 9
+// Charecter that marks difference in temps
+#define TEMP_DIVIDER "|"
+// Target temperature of tanks
+#define TEMP_TARGET 63.0
+
+// Packet Identifiers
+#define PACKET_TEMP_IDEN "T"
+#define PACKET_LUX_IDEN "L"
+
 #include <BH1750.h>
 
 BH1750 GY302; // initialize BH1750 object
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-byte zero[] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000
-};
-byte one[] = {
-  B10000,
-  B10000,
-  B10000,
-  B10000,
-  B10000,
-  B10000,
-  B10000,
-  B10000
-};
-byte two[] = {
-  B11000,
-  B11000,
-  B11000,
-  B11000,
-  B11000,
-  B11000,
-  B11000,
-  B11000
-};
-byte three[] = {
-  B11100,
-  B11100,
-  B11100,
-  B11100,
-  B11100,
-  B11100,
-  B11100,
-  B11100
-};
-byte four[] = {
-  B11110,
-  B11110,
-  B11110,
-  B11110,
-  B11110,
-  B11110,
-  B11110,
-  B11110
-};
-byte five[] = {
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111
-};
+// Include if your want to reinclude loading screen
+// #include "lc_chars.h"
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd2(0x26,20,4);
 // Setup a oneWire instance to communicate with any OneWire device
+
+/* Old one wire setup, why was it written like this?
+
 OneWire oneWire(TEMPB);  
 OneWire oneWire2(TEMPC);  
 OneWire oneWire3(TEMPD);  
 OneWire oneWire4(TEMPA);  
-bool initialized = false;
+bool initialized = false; 
 // Pass oneWire reference to DallasTemperature library
 DallasTemperature atank(&oneWire4);
 DallasTemperature btank(&oneWire);
 DallasTemperature ctank(&oneWire2);
 DallasTemperature dtank(&oneWire3);
-byte customChar[] = {
-  B00100,
-  B01010,
-  B00100,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000
-};
-byte customCharSmile[] = {
-  B01010,
-  B01010,
-  B01010,
-  B00000,
-  B00000,
-  B00000,
-  B10001,
-  B01110
-};
+
+*/
+// New one wire setup
+OneWire oneWire_A(TEMPA);  
+OneWire oneWire_B(TEMPB);  
+OneWire oneWire_C(TEMPC);  
+OneWire oneWire_D(TEMPD);  
+bool initialized = false; 
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature atank(&oneWire_A);
+DallasTemperature btank(&oneWire_B);
+DallasTemperature ctank(&oneWire_C);
+DallasTemperature dtank(&oneWire_D);
+
 void setup(void)
 {
   lcd.init();
-  lcd2.init();
-  lcd2.backlight();
-  GY302.begin();
-//  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+
+  /* idk if this is needed
   lcd.createChar(0, zero);
   lcd.createChar(1, one);
   lcd.createChar(2, two);
   lcd.createChar(3, three);
   lcd.createChar(4, four);
   lcd.createChar(5, five);
+  */
   
-  // Print a message to the LCD.
-  lcd.backlight();
-  btank.begin();  // Start up the library
+  lcd2.init();
+  lcd2.backlight();
+  
+ 
+  // Start up the libraries
+  atank.begin();  
+  btank.begin();  
   ctank.begin();
-  atank.begin();  // Start up the library
   dtank.begin();
-  Serial.begin(9600);
+  GY302.begin();
+
+  // Designates IO device ports
+  
   pinMode(PIN_RELAY_1, OUTPUT);
   pinMode(PIN_RELAY_2, OUTPUT);
   pinMode(PIN_RELAY_3, OUTPUT);
   pinMode(PIN_RELAY_4, OUTPUT);
-  //lcd.createChar(11, customChar);
-  //lcd.createChar(12, customCharSmile);
 
+  // Starts serial
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
 }
 void updateProgressBar(unsigned long count, unsigned long totalCount, int lineToPrintOn) {
     double factor = totalCount/80.0;          
@@ -164,20 +122,6 @@ void updateProgressBar(unsigned long count, unsigned long totalCount, int lineTo
       }
     }  
  }
-void TempBank(){
-  btank.requestTemperatures();
-  ctank.requestTemperatures();
-  atank.requestTemperatures();
-  dtank.requestTemperatures();
-  String BTEMP = (String("B"+(String((btank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0)))+"F" +" ");
-  String CTEMP = (String("C"+(String((ctank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0)))+"F" +" ");
-  String ATEMP = (String("A"+(String((atank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0)))+"F" +" ");
-  String DTEMP = (String("D"+(String((dtank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0)))+"F" +" ");
-  lcd.setCursor(0,0);
-  lcd.print(String(ATEMP+BTEMP));
-  lcd.setCursor(0,1);
-  lcd.print(String(CTEMP+DTEMP));
-}
 
 void InitLoop(){
   updateProgressBar(millis(), 15000, 1);
@@ -208,76 +152,101 @@ void InitLoop(){
 
 }
 
-void RelayBank(){
-  String AState = "ON";
-  String BState = "ON";
-  String CState = "ON";
-  String DState = "ON";
-  if(digitalRead(PIN_RELAY_1) == HIGH){
-    AState = "OFF ";
-  }
-  if(digitalRead(PIN_RELAY_2) == HIGH){
-    BState = "OFF ";
-  }
-  if(digitalRead(PIN_RELAY_3) == HIGH){
-    CState = "OFF ";
-  }
-  if(digitalRead(PIN_RELAY_4) == HIGH){
-    DState = "OFF ";
-  }
-  lcd2.clear();
-  lcd2.setCursor(0,0);
-  lcd2.print(String("A:"+String(AState)+" B:"+String(BState)));
-  lcd2.setCursor(0,1);
-  lcd2.print(String("C:"+String(CState)+" D:"+String(DState)));
-}
-void TempLoop(float temp){
+
+void reqTemps(float &ATEMP, float &BTEMP, float &CTEMP, float &DTEMP) {
+  atank.requestTemperatures();
   btank.requestTemperatures();
   ctank.requestTemperatures();
-  atank.requestTemperatures();
   dtank.requestTemperatures();
-  if((btank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0 > temp){
-    digitalWrite(PIN_RELAY_2, LOW);
-  }
-  else{
-    digitalWrite(PIN_RELAY_2, HIGH);
-  }
-  if((atank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0 > temp){
-    digitalWrite(PIN_RELAY_1, LOW);
-  }
-  else{
-    digitalWrite(PIN_RELAY_1, HIGH);
-  }
-  if((ctank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0 > temp){
-    digitalWrite(PIN_RELAY_3, LOW);
-  }
-  else{
-    digitalWrite(PIN_RELAY_3, HIGH);
-  }
-  if((dtank.getTempCByIndex(0) * 9.0) / 5.0 + 32.0 > temp){
-    digitalWrite(PIN_RELAY_4, LOW);
-  }
-  else{
-    digitalWrite(PIN_RELAY_4, HIGH);
-  }
+
+  ATEMP = atank.getTempFByIndex(0);
+  BTEMP = btank.getTempFByIndex(0);
+  CTEMP = ctank.getTempFByIndex(0);
+  DTEMP = dtank.getTempFByIndex(0);
 }
+
+// Simple checksum for data integrity
+// Adapted from: 
+// - https://forum.arduino.cc/t/simple-checksum-that-a-noob-can-use/300443
+// - https://www.techiedelight.com/iterate-over-characters-string-cpp/
+char checksum(const String &s)
+{
+    byte ch = 0;
+    for (char const &c: s) {ch ^= c;} 
+    return ch;
+}
+
+// Function for sending packet data w/ integrity checksum
+// Iden is a 1 charecter long string meant to distinguish packets
+void sendSerialPacket(const String &msg, const String &iden) {
+  const String msg_with_iden = iden[0] + msg;                    // Pads message with identifying tags
+  Serial.println(msg_with_iden + "C" + checksum(msg_with_iden)); // Sends Message
+}
+
+
+void TempBank(){
+  float _ATEMP, _BTEMP, _CTEMP, _DTEMP;
+  reqTemps(_ATEMP, _BTEMP, _CTEMP, _DTEMP);
+
+  // Convert temperatures to string
+  String ATEMP = String(_ATEMP, 2); 
+  String BTEMP = String(_BTEMP, 2);
+  String CTEMP = String(_CTEMP, 2);
+  String DTEMP = String(_DTEMP, 2);
+
+  // String representation of temps
+  String ATEMP_FORMATTED = "A" + ATEMP + "F" + " ";
+  String BTEMP_FORMATTED = "B" + BTEMP + "F" + " ";
+  String CTEMP_FORMATTED = "C" + CTEMP + "F" + " ";
+  String DTEMP_FORMATTED = "D" + DTEMP + "F" + " ";
+
+  lcd.setCursor(0,0);
+  lcd.print(String(ATEMP_FORMATTED + BTEMP_FORMATTED));
+  lcd.setCursor(0,1);
+  lcd.print(String(CTEMP_FORMATTED + DTEMP_FORMATTED));
+
+  // Construct message to be sent over USB
+  String SERIAL_MSG = String(ATEMP) + TEMP_DIVIDER +
+                      String(BTEMP) + TEMP_DIVIDER +  
+                      String(CTEMP) + TEMP_DIVIDER +
+                      String(DTEMP);
+
+  // Send temperature data over serial 
+  sendSerialPacket(SERIAL_MSG, PACKET_TEMP_IDEN);
+}
+
+void TempLoop(float temp){
+  float ATEMP, BTEMP, CTEMP, DTEMP;
+  reqTemps(ATEMP, BTEMP, CTEMP, DTEMP);
+
+  ATEMP > temp ? digitalWrite(PIN_RELAY_1, LOW) : digitalWrite(PIN_RELAY_1, HIGH);
+  BTEMP > temp ? digitalWrite(PIN_RELAY_2, LOW) : digitalWrite(PIN_RELAY_2, HIGH);
+  CTEMP > temp ? digitalWrite(PIN_RELAY_3, LOW) : digitalWrite(PIN_RELAY_3, HIGH);
+  DTEMP > temp ? digitalWrite(PIN_RELAY_4, LOW) : digitalWrite(PIN_RELAY_4, HIGH);
+}
+
+void RelayBank(){
+  String AState, BState, CState, DState;
+
+  digitalRead(PIN_RELAY_1) == HIGH ? AState = "OFF" : AState = "ON";
+  digitalRead(PIN_RELAY_2) == HIGH ? BState = "OFF" : BState = "ON";
+  digitalRead(PIN_RELAY_3) == HIGH ? CState = "OFF" : CState = "ON";
+  digitalRead(PIN_RELAY_4) == HIGH ? DState = "OFF" : DState = "ON";
+
+  lcd2.clear();
+  lcd2.setCursor(0,0);
+  lcd2.print("A:" + String(AState) + " B:" + String(BState));
+  lcd2.setCursor(0,1);
+  lcd2.print("C:" + String(CState) + " D:" + String(DState));
+}
+
+
 void loop(void)
 {
+  TempBank();             // Sends & prints temps
+  TempLoop(TEMP_TARGET);  // Sets relay state based on temp
+  RelayBank();            // Prints relay state
   
-  if(initialized){
-    TempBank();
-    RelayBank();
-    TempLoop(63);
-    //lcd2.print("HI:");
-    // get reading from module
-    uint16_t lux = GY302.readLightLevel();
-    
-    // display to Serial Monitor
-    Serial.print("Light: ");
-    Serial.print(lux);
-    Serial.println(" lx");
-  } 
-  else{
-    InitLoop();
-  }
+  // get reading from light module, send over serial
+  sendSerialPacket(String(GY302.readLightLevel()), PACKET_LUX_IDEN);
 }
